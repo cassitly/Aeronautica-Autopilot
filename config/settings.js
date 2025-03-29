580b064984a2bcc5c5dec2ca679a76542f59787b
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-03-29 19:15:49 +0700

    Added more settings

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,8 @@
 let maxAirspeed = 467; // Set Max Airspeed
 let minAirspeed = 200; // Set Min Airspeed
 
-let maxAltitude = 10000; // Maximum altitude
-let minAltitude = 5000; // Minimum altitude
+let maxAltitude = 1000; // Maximum altitude
+let minAltitude = 500; // Minimum altitude
 
 let maxThrottle = 100; // Maximum speed percentage of the plane
 let minThrottle = 0; // Stall speed percentage of the plane
@@ -13,7 +13,8 @@ let minDistance = 10 // Minimium distance from destination until alerting
 let maxVerticalSpeed = 100; // Max vertical speed range
 let minVerticalSpeed = 0; // Minimium vertical speed range
 
-let imageLatency = 100
+let imageLatency = 5000; // Image latency, for rate limiting the OCR recognition, recommended 10 seconds (aka 10,000 in miliseconds)
+let retryLatency = 5000; // Retry latency, for in case the image latency system fails, recommended 5 seconds (aka 5,000 in miliseconds)
 
 let useThrottle = false; // Tells the application to use the throttle indicator for speed
 let useAirspeed = true; // Tells the application to use the airspeed indicator for speed
@@ -40,4 +41,5 @@ exports['settings'] = {
     useBoth,
     useFlightPlan,
     shareAnonymous,
+    retryLatency,
 }
